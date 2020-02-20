348c34b458bcfeac081e47c4d306624a1b2b094d
salmankhan1995 <p.salmankhan1995@gmail.com>
2020-02-20 12:27:05 +0530

    first commit of sidebar

---
+++ src/components/salmankhan.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Imtiyaz(){
+    return(
+        <div>
+            <h1>salman khan </h1>
+            <p>being human</p>
+        </div>
+
+    )
+        
+    
+}
+export default Imtiyaz;
---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Kushwanth extends Component {
+    render(){
+        return(
+            <div>
+                <h1>kushwanth..liojsgskdj</h1>
+                <p>hdjddbckhejd</p>
+            </div>
+        )
+    }
+}
+export default Kushwanth;
